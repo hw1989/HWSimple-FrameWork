b2254bd7b7dd352568fd058093572e39ffdfec6d
hw1989 <hw_hw1989@163.com>
2015-06-17 18:48:49 +0800

    update

--- Android-Control-View/src/org/wind/calendarview/TableView.java
+++ Android-Control-View/src/org/wind/calendarview/TableView.java
@@ -6,37 +6,46 @@ import android.util.AttributeSet;
 import android.view.View;
 
 public class TableView extends View {
-	//横向的线数
-    private int Hlines=25;
-    //纵向的线数
-    private int Vlines=8;
-    //单元格的高度
-    private int cellhei=80;
-    //文字的间距
-    private int padd=8;
+	// 横向的线数
+	private int Hlines = 25;
+	// 纵向的线数
+	private int Vlines = 8;
+	// 单元格的高度
+	private int cellhei = 80;
+	// 文字的间距
+	private int padd = 8;
+    //
+	private int txtwid=0;
 	public TableView(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
 		init();
 	}
-    
+
 	public TableView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		init();
 	}
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-    	super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-    }
+
 	public TableView(Context context) {
 		super(context);
 		init();
 	}
-    private void init(){
-    	
-    }
+
+	private void init() {
+        
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		// super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		int height = cellhei * (Hlines - 1);
+		super.onMeasure(widthMeasureSpec,
+				MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY));
+	}
+
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-		
+
 	}
 }
