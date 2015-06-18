0debc27063c8a1d9f79e19b5da37efa8e44c1eca
hw1989 <hw_hw1989@163.com>
2015-06-18 18:16:41 +0800

    update calendarview

--- Android-Control-View/src/org/wind/calendarview/CanlendarView.java
+++ Android-Control-View/src/org/wind/calendarview/CanlendarView.java
@@ -2,30 +2,238 @@ package org.wind.calendarview;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Point;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.Scroller;
 
-public class CanlendarView extends ViewGroup {
+public class CanlendarView extends FrameLayout {
+	private Scroller scroller = null;
+	private VelocityTracker tracker = null;
+	private ViewConfiguration configuration = null;
+	private TableView table1 = null;
+	private LayoutParams params = null;
+	private int tablewid;
+	private int tablehei;
+	// 触摸开始处理距离
+	private int touchlen = 15;
+	private GestureDetector detector = null;
+
+	private enum Direction {
+		None, Horizontal, Vertical
+	};
+
+	// 最大滑动速度
+	private int maxspeed;
+	// 最小滑动速度
+	private int minspeed;
+	private int moveMaxY;
+	private Direction dir = Direction.None;
+	private GestureDetector.SimpleOnGestureListener simple = new GestureDetector.SimpleOnGestureListener() {
+		private int xlen = 0;
+		private int ylen = 0;
+
+		@Override
+		public void onLongPress(MotionEvent e) {
+			super.onLongPress(e);
+		}
+
+		@Override
+		public boolean onScroll(MotionEvent e1, MotionEvent e2,
+				float distanceX, float distanceY) {
+			// 判断是否确定这次滑动的方向
+			// 不确定，就等待确定的条件
+			// 确定，就执行滑动的距离
+			if (dir == Direction.None) {
+				xlen += Math.abs(distanceX);
+				ylen += Math.abs(distanceY);
+				if (xlen - ylen >= touchlen) {
+					dir = Direction.Horizontal;
+				}
+				if (ylen - xlen >= touchlen) {
+					dir = Direction.Vertical;
+				}
+			}
+			if (dir == Direction.Horizontal) {
+				scrollBy((int) distanceX, 0);
+			} else if (dir == Direction.Vertical) {
+				scrollBy(0, (int) distanceY);
+			}
+			return true;
+		}
+
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+				float velocityY) {
+			float abs_velocityX = Math.abs(velocityX);
+			float abs_velocityY = Math.abs(velocityY);
+			if (dir == Direction.Horizontal) {
+				if (abs_velocityX > minspeed && abs_velocityX < maxspeed) {
+					scroller.fling((int) e1.getX(), 0, (int) -velocityX, 0,
+							-tablewid, tablewid, 0, 0);
+					awakenScrollBars(scroller.getDuration());
+					invalidate();
+				}
+			} else if (dir == Direction.Vertical) {
+				if (abs_velocityY > minspeed && abs_velocityY < maxspeed) {
+					Log.e("info", "speed   " + abs_velocityY + "   ");
+					scroller.fling(0, (int) e1.getY(), 0, (int) -abs_velocityY,
+							0, 0, 0, moveMaxY);
+					awakenScrollBars(scroller.getDuration());
+					invalidate();
+				}
+			}
+			return true;
+		}
+
+		@Override
+		public boolean onDown(MotionEvent e) {
+			dir = Direction.None;
+			// 初始化，用于下次的判断
+			xlen = 0;
+			ylen = 0;
+			return true;
+		}
+
+	};
 
 	public CanlendarView(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
+		init();
 	}
 
 	public CanlendarView(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		init();
 	}
 
 	public CanlendarView(Context context) {
 		super(context);
+		init();
 	}
-    @Override
-    protected void onDraw(Canvas canvas) {
-    	super.onDraw(canvas);
-    }
+
+	private void init() {
+		configuration = ViewConfiguration.get(getContext());
+		scroller = new Scroller(getContext());
+		detector = new GestureDetector(getContext(), simple);
+		table1 = new TableView(getContext());
+		params = new LayoutParams(LayoutParams.MATCH_PARENT,
+				LayoutParams.MATCH_PARENT);
+		this.addView(table1);
+		maxspeed = configuration.getScaledMaximumFlingVelocity();
+		minspeed = configuration.getScaledMinimumFlingVelocity();
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+	}
 
 	@Override
 	protected void onLayout(boolean changed, int l, int t, int r, int b) {
-		
+		table1.layout(0, 0, table1.getMeasuredWidth(),
+				table1.getMeasuredHeight());
+		tablehei = table1.getMeasuredHeight();
+		tablewid = table1.getMeasuredWidth();
+		moveMaxY = Math.abs(getHeight() - tablehei);
+	}
+
+	private Point point = null;
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		// boolean flag= detector.onTouchEvent(event);
+		if (event.getAction() == MotionEvent.ACTION_DOWN) {
+			dir = Direction.None;
+			point = new Point((int) event.getX(), (int) event.getY());
+			initTracker(event);
+		} else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+			int disx = (int) event.getX() - point.x;
+			int disy = (int) event.getY() - point.y;
+			if (dir == Direction.None) {
+				if (Math.abs(disx) > Math.abs(disy) && Math.abs(disx) > 20) {
+					dir = Direction.Horizontal;
+				}
+				if (Math.abs(disy) > Math.abs(disx) && Math.abs(disy) > 20) {
+					dir = Direction.Vertical;
+				}
+			}
+			if (dir == Direction.Horizontal) {
+                //两个临界值(1.最顶部继续向上滑动)(2.最底部继续下滑)
+				if (getScaleY() >= moveMaxY && disy > 0) {
+//					scrollTo(0, moveMaxY);
+				}else if(getScaleY()==0&&disy<0){
+//					scrollTo(0, 0);
+				}else{
+					scrollBy(-disx, 0);
+				}
+				point.set((int) event.getX(), (int) event.getY());
+			} else if (dir == Direction.Vertical) {
+				scrollBy(0, -disy);
+				point.set((int) event.getX(), (int) event.getY());
+			}
+			point.set((int) event.getX(), (int) event.getY());
+			tracker.addMovement(event);
+		} else if (event.getAction() == MotionEvent.ACTION_CANCEL
+				|| event.getAction() == MotionEvent.ACTION_UP) {
+			// 垂直方向的fling
+			if (dir == Direction.Vertical) {
+				tracker.computeCurrentVelocity(1000, maxspeed);
+				int initialVelocity = (int) tracker.getYVelocity();
+				if (Math.abs(initialVelocity) > minspeed) {
+					scroller.fling(0, (int) event.getY(), 0, -initialVelocity,
+							0, getWidth(), 0 , moveMaxY );
+					awakenScrollBars(scroller.getDuration());
+					invalidate();
+				}
+			}
+
+			destoryTracker();
+		}
+		return true;
+	}
+
+	/**
+	 * 初始化 VelocityTracker
+	 */
+	private void initTracker(MotionEvent event) {
+		if (tracker == null) {
+			tracker = VelocityTracker.obtain();
+		} else {
+			tracker.clear();
+		}
+		tracker.addMovement(event);
+	}
+
+	/**
+	 * 释放 VelocityTracker
+	 */
+	private void destoryTracker() {
+		if (tracker != null) {
+			tracker.recycle();
+			tracker = null;
+		}
+	}
+
+	@Override
+	public void computeScroll() {
+		super.computeScroll();
+		if (scroller.computeScrollOffset()) {
+			scrollTo(scroller.getCurrX(), scroller.getCurrY());
+			postInvalidate();
+		} else {
+			// if(){
+			//
+			// }else if(){
+			//
+			// }
+		}
 	}
 }
--- Android-Control-View/src/org/wind/calendarview/TableView.java
+++ Android-Control-View/src/org/wind/calendarview/TableView.java
@@ -1,21 +1,37 @@
 package org.wind.calendarview;
 
+import java.text.DecimalFormat;
+
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class TableView extends View {
+	// 控件的宽度
+	private int width = 0;
 	// 横向的线数
 	private int Hlines = 25;
 	// 纵向的线数
 	private int Vlines = 8;
 	// 单元格的高度
-	private int cellhei = 80;
+	private int cellhei = 200;
+	private int cellwid = 0;
 	// 文字的间距
-	private int padd = 8;
-    //
-	private int txtwid=0;
+	private int paddtxt = 8;
+	//
+	private int txtwid = 0;
+	// 距离上边界和右边界的距离
+	private int paddlr = 8;
+	private int paddtb = 0;
+	// 文本画笔
+	private Paint paint_txt = null;
+	// 线的画笔
+	private Paint paint = null;
+    private DecimalFormat format=null;
 	public TableView(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
 		init();
@@ -32,7 +48,18 @@ public class TableView extends View {
 	}
 
 	private void init() {
-        
+		format=new DecimalFormat("00");
+		paint_txt = new Paint(Paint.ANTI_ALIAS_FLAG);
+		paint_txt.setAntiAlias(true);
+		paint_txt.setTextSize(25);
+		Rect rect = new Rect();
+		paint_txt.getTextBounds("00:00", 0, "00:00".length(), rect);
+		txtwid = rect.width();
+		//
+		paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		paint.setAntiAlias(true);
+		paint.setColor(Color.GRAY);
+		paint.setStrokeWidth(2f);
 	}
 
 	@Override
@@ -41,11 +68,38 @@ public class TableView extends View {
 		int height = cellhei * (Hlines - 1);
 		super.onMeasure(widthMeasureSpec,
 				MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY));
+		width = MeasureSpec.getSize(widthMeasureSpec);
+		// (总宽度-文字和文字左右边距-控件的右边距)/一周的时间
+		cellwid = (width - (paddtxt * 2 + txtwid) - paddlr) / (Vlines - 1);
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
+		drawHorizontal(canvas);
+		drawVertical(canvas);
+	}
 
+	/**
+	 * 画横向的线
+	 */
+	private void drawHorizontal(Canvas canvas) {
+		for (int i = 0; i < Hlines; i++) {
+			canvas.drawLine(paddtxt * 2 + txtwid, paddtb + i * cellhei, width
+					- paddlr, paddtb + i * cellhei, paint);
+			//画文字
+			canvas.drawText(format.format(i)+":00", paddtxt, paddtb + i * cellhei, paint_txt);
+		}
+	}
+
+	/**
+	 * 画垂直的线
+	 */
+	private void drawVertical(Canvas canvas) {
+		for (int i = 0; i < Vlines; i++) {
+			canvas.drawLine(paddtxt * 2 + txtwid + (i * cellwid), paddtb,
+					paddtxt * 2 + txtwid + (i * cellwid), paddtb + Hlines
+							* cellhei, paint);
+		}
 	}
 }
