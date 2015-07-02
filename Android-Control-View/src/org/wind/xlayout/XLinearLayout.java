4467ff9d5bcac6d8caf9863c4c78f657e6353a1b
hw1989 <hw_hw1989@163.com>
2015-07-02 19:10:55 +0800

    添加下拉layout

---
+++ Android-Control-View/src/org/wind/xlayout/XLinearLayout.java
@@ -0,0 +1,24 @@
+package org.wind.xlayout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class XLinearLayout extends LinearLayout {
+
+	public XLinearLayout(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+	public XLinearLayout(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public XLinearLayout(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
