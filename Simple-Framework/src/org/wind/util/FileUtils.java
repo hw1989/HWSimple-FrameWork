5b419f96934cdce433aab7f63c0b3238479f5e75
hw1989 <hw_hw1989@163.com>
2015-07-10 18:09:31 +0800

    添加文件类

---
+++ Simple-Framework/src/org/wind/util/FileUtils.java
@@ -0,0 +1,21 @@
+package org.wind.util;
+
+import java.io.File;
+
+public class FileUtils {
+	/**
+	 * 判断路径是否存在
+	 * 
+	 * @param path验证路径
+	 * @param create创建路径
+	 */
+	public static boolean createorexistsPath(String path, boolean create) {
+		File file = new File("");
+		if (file.exists()) {
+			file.mkdirs();
+			return false;
+		} else {
+			return true;
+		}
+	}
+}
